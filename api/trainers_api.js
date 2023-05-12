b0c1708aa420a093964a2a8e5aca985e1a6339b5
VickyMontanezCampus <vmontanez707@gmail.com>
2023-05-12 09:36:27 -0500

    feat: :rocket: First ry

---
+++ api/trainers_api.js
@@ -0,0 +1,55 @@
+const url = "http://localhost:4000";
+
+export default {
+/* Funcion para que al trainer añada reclutas*/  
+    async addRecluta(user) {
+        try {
+            let addData = await (await fetch(`${url}/reclutas`, {
+                method:"POST",
+                body: JSON.stringify(user),
+                headers:{"Content-Type":"application/json"}
+            })).json();
+            return addData
+        } catch (error) {
+            console.log(error);
+        }
+    },
+/* Funcion para que al trainer se le muestren los reclutas*/
+    async showRecluta(){
+        try {
+            let config = {
+                method:"GET",
+                header:{"Content-Type":"application/json"},
+            };
+            let showData = await fetch(`${url}/reclutas`,config);
+            let results = await showData.json();
+            console.log(results);
+        } catch (error) {
+            console.log(error);
+        }
+    },
+
+/* Funcion para que al trainer elimine reclutas*/
+    async deleteRecluta() {
+        try {
+            await fetch(`${url}/reclutas`,{method: "DELETE"});
+        } catch (error) {
+            console.log(error);
+        }
+    },
+/* Funcion para que al trainer añada notas los reclutas*/
+    async addNota(nota) {
+        try {
+            let addDataNota = await (await fetch(`${url}/notas`, {
+                method:"POST",
+                body: JSON.stringify(nota),
+                headers:{"Content-Type":"application/json"}
+            })).json();
+            return addDataNota
+        } catch (error) {
+            console.log(error);
+        }
+    },
+}
+
+
---
+++ js/trainers.js
@@ -0,0 +1,21 @@
+import trainers_api from "../api/trainers_api.js";
+
+let formReclutas = document.querySelector("#reclutas");
+
+
+formReclutas.addEventListener("submit", async(e)=>{
+    e.preventDefault();
+    let formData = Object.fromEntries(new FormData(e.target));
+    let data = await trainers_api.addRecluta(formData);
+    console.log(data)
+    formReclutas.reset();
+});
+
+let formEvaluacion = document.querySelector("#evaluacion");
+formEvaluacion.addEventListener("submit", async(e)=>{
+    e.preventDefault();
+    let formNotas = Object.fromEntries(new FormData(e.target));
+    let data = await trainers_api.addNota(formNotas);
+    console.log(data);
+    formEvaluacion.reset();
+});
